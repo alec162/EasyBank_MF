d52903f1a87c0d4ce8b1a841d82b8217a467b09b
alec162 <105893227+alec162@users.noreply.github.com>
2022-06-13 18:05:15 -0500

    tried toggle

--- App/JS/script.js
+++ App/JS/script.js
@@ -2,11 +2,5 @@ const btnHamburger = document.querySelector('#btnHamburger');
 
 btnHamburger.addEventListener('click', function(){
     console.log('click hamburger');
-
-    if(btnHamburger.classList.contains('open')){
-        btnHamburger.classList.remove('open');
-    }
-    else {
-        btnHamburger.classList.add('open');
-    }
-});
+    btnHamburger.classList.toggle('open');
+}
